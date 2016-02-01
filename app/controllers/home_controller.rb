07183acd6a867270c34e5f6609ff71dba0e3ed9e
Takahoro Kawakami <takabo.beta@gmail.com>
2016-02-01 12:17:30 +0000

    modified devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,9 @@
 class HomeController < ApplicationController
-  
+
   #before_action :authenticate_user!, only: :show
-  
+
   def index
     @scores = Score.all
-    if @scores.length > 0
-      @score = Score.find(1)
-    end
-    
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,25 @@
 Rails.application.routes.draw do
-  
+
   root 'home#index'
-  
-  devise_for :users
-  
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations'
+  }
+
   get  'inquiry'          => 'inquiry#index'
   post 'inquiry/confirm'  => 'inquiry#confirm'
   post 'inquiry/complete' => 'inquiry#complete'
-  
+
   resources :scores
-  
+
   get  'mario' => 'mario#index'
   get  'mario2' => 'mario#index2'
-  
+
   get '/users'     => 'users#index', as:'users'
   get '/:username' => 'users#show',  as:'user'#!最後に設置
-  
+
   #only:[:index,:show,:new,:create,:edit,:update,:destroy]
 end
